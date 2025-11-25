de8a0978e96e5110a1227b8eacec0ac4293e8c8d
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-11-25 16:24:15 +0100

    feat: add lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "tokyonight",
+        section_separators = "",
+        component_separators = "",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { "filename" },
+        lualine_x = { "diagnostics" },
+        lualine_y = { "filetype" },
+        lualine_z = { "location" },
+      },
+    },
+  },
+}
