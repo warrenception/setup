f4ddb49121d661ac9c8dad47702587fda10ee30d
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-12-09 08:46:16 +0100

    chore(nvim): replace nvim-tree with oil.nvim

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,28 +1,50 @@
 return {
   {
-    "nvim-tree/nvim-tree.lua",
+    "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<D-b>", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file tree" },
-    },
-    opts = {
-      view = {
-        width = 35,
-        number = false,
-        relativenumber = false,
-      },
-      renderer = {
-        icons = {
-          show = {
-            git = true,
-            folder = true,
-            file = true,
-          },
+    config = function()
+      local oil = require("oil")
+
+      local function open_in_editor()
+        local entry = oil.get_cursor_entry()
+        if entry and entry.type == "file" then
+          local filepath = oil.get_current_dir() .. entry.name
+          for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            if vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "oil" then
+              vim.api.nvim_set_current_win(win)
+              vim.cmd("edit " .. vim.fn.fnameescape(filepath))
+              return
+            end
+          end
+        end
+        oil.select()
+      end
+
+      local function toggle_sidebar()
+        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+          if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == "oil" then
+            vim.api.nvim_win_close(win, true)
+            return
+          end
+        end
+        vim.cmd("vsplit | wincmd H | vertical resize 35")
+        oil.open()
+      end
+
+      oil.setup({
+        columns = { "icon" },
+        view_options = { show_hidden = true },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<C-l>"] = false,
+          ["<C-j>"] = false,
+          ["<C-k>"] = false,
+          ["<CR>"] = { callback = open_in_editor, desc = "Open in editor" },
         },
-      },
-      filters = {
-        dotfiles = false,
-      },
-    },
+      })
+
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      vim.keymap.set("n", "<D-b>", toggle_sidebar, { desc = "Toggle file browser" })
+    end,
   },
 }
